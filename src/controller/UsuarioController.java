d6b42750955c64af8bab8ebe4eb5f0160dc644b3
Weskley Bezerra <weskleymb@hotmail.com>
2016-07-24 12:05:54 -0300

    Commit

---
+++ src/controller/UsuarioController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import dao.UsuarioDAO;
+import model.Usuario;
+
+public class UsuarioController {
+    
+    private final UsuarioDAO dao;
+    
+    public UsuarioController() {
+        dao = new UsuarioDAO();
+    }
+    
+    public boolean cadastrar(Usuario usuario) {
+        if (dao.findByLogin(usuario.getLogin()) == null) {
+            if (dao.create(usuario)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean login(String login, String senha) {
+        Usuario usuario = dao.findByLogin(login);
+        if (usuario.getLogin().equals(login) && usuario.getSenha().equals(senha)) {
+            return true;
+        }
+        return false;
+    }
+    
+}
